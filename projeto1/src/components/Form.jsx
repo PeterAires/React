181439c3cb0b8c1f93dffb4b624e0e54d6dfd7b3
PeterAires <168139612+PeterAires@users.noreply.github.com>
2024-05-08 19:44:43 -0300

    treino react diario

--- projeto1/src/components/Form.jsx
+++ projeto1/src/components/Form.jsx
@@ -1,14 +1,27 @@
+import { useState } from "react"
+
 function Form(){
  function cadastrarUsuario(e) {
     e.preventDefault() 
-    console.log('Usuario Cadastrado')
- }
+    console.log(`usuario ${name} foi cadaastrado com a senha ${password}`)
+}  
+ const [password,SetPassword] = useState()
+ const [name,SetName] = useState() // a variavel inicial, o componente capaz de mudar essa variavel, um valor inicial quando o componente for inicializado ,,, la em baixo// event é bacicamente a caixa, o ato de escrever, o SetName vai alterar o valor de 'nome' que começa vazio, para o que digitamos na caixa ,que é o evento, pegamos o target e o value e ai sim, alteramos ele letra por letra pelo set name,,, set name para atribuir o valor a 'name'
+
  return(
     <div>
         <h1>meu cadastro</h1>
         <form onSubmit={cadastrarUsuario}>
             <div>
-                <input type="text" placeholder="Digite o seu nome"/>
+                <label htmlFor="name" id="name" placeholder='Digite o seu nome'>name:</label>
+                <input type="text" placeholder="Digite o seu nome"
+                onChange={(event) => SetName(event.target.value)}
+                /> 
+            </div>
+            <div>
+                <label htmlFor="password">Senha:</label>
+                <input type="password" id="password" placeholder="Digite a sua senha" 
+                onChange={(event) => SetPassword(event.target.value)}/>
             </div>
             <div>
                 <input type="submit" value='Cadastrar' />
