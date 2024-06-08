bc526c5b24d4ef593331110fa02aad9c69772493
PeterAires <168139612+PeterAires@users.noreply.github.com>
2024-06-08 15:41:30 -0300

    treino diario
    estou ficando maluco

--- projeto1/src/App.jsx
+++ projeto1/src/App.jsx
@@ -6,39 +6,45 @@ function App(){
   //dois useStates: um pega o que vc escreveu e o outro salva na lista do useState
 
 
-  const [valorEscrito,setValorEscrito] = useState('')
+  const [Titulo,setTitulo] = useState('')
+  const [Descricao,setDescricao] = useState('')
   const [Tarefas,setTarefas] = useState([])
-
-  //primeiro passo: pegar o que a pessoa escreveu e mandar pro 'valorEscrito'
-  const valorDigitado = (e) => {
-    setValorEscrito(e.target.value)
-  }
+  
   
 //se no valor escrito nao tiver nada ele nao vai adicionar a lista,,,Spread operator, (adicionar elemento a lista),,,trim(remove os espaços em branco no inicio e fim),,,zerar o valor que esta salvo para evitar repetição de tarefas
   function AdicionarTarefa(){
-    if (valorEscrito.trim() != ''){
-      setTarefas([...Tarefas, valorEscrito.trim()])
-      setValorEscrito('')
+    if (Titulo.trim() != '' && Descricao.trim() != '' ){
+      //crebei a cabeça aqui, mas depois de pensar um pouco era so criar outro array
+      const novaTarefa = [<h3>{Titulo}</h3>,<br/>,<h4>{Descricao}</h4>] 
+      setTarefas([...Tarefas, novaTarefa])
+      setTitulo('')
+      setDescricao('')
     }
     else{window.alert('Erro, não há nada digitado ou Esta tarefa ja foi Adicionada')}
   }
+  function Excluir(c){
+    const remover = Tarefas.indexOf(Tarefas[c])
+    if (remover !== -1) {
+      Tarefas.splice(remover, 1);
 
-  
+   }}
 //retornamos tudo e no final utilizamos a função map para mapear as tarefas
   return (
     <div>
       <h1>Gerenciador de tarefas</h1>
 
-      <input type="text" placeholder='Digite uma tarefa' onChange={valorDigitado} />
+      <input type="text" placeholder='Titulo' onChange={(e) => setTitulo(e.target.value)} />
+
+      <input type="text" placeholder='Descrição' onChange={(e) => setDescricao(e.target.value)}/>
 
       <button onClick={AdicionarTarefa}>Adicionar</button>
+      <h2>Minhas tarefas</h2>
       <ul>
-        {Tarefas.map((t) => (
-          <p>{t}</p>
-        ))}
+      
+       {Tarefas.map((t, c = 0) => (<div>{t} <button onClick={(c) => Excluir(Tarefas.c)}>X</button></div>)
+       )}
       </ul>
     </div>
   )
-  
 }
 export default App
